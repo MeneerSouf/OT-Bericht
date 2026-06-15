830ec5590b14b8ef9b6b558285cd699cb2d13cb9
MeneerSouf <soufyt2@gmail.com>
2026-06-15 20:18:20 +0200

    Added new files

---
+++ ot-bericht-plugin/message.ts
@@ -0,0 +1,17 @@
+export default {
+    message: `**Naam**:
+**Leeftijd**:
+**Ervaring:**
+**Motivatie:**
+**Kan jij je Gedragen?:**
+
+**Mail**:
+**Telefoon Nummer (Indien je deze wilt geven)**:
+
+**Waarom wil je hier werken?**:
+**Wat zou jij kunnen toevoegen aan ons Team?**:
+
+**Begrijp je dat je ten alle tijden kan ontslagen worden?**:
+
+*Wist je al dat je onze **TOS** moet lezen?*`
+}
